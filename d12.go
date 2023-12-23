db1314c1e326fc14cd4694aac3e7fe1806bce91c
Meelis Utt <meelis.utt@gmail.com>
2023-12-23 16:34:52 +0100

    d12.go: cleaned up version for e1

--- d12.go
+++ d12.go
@@ -5,85 +5,10 @@ import (
 	_ "embed"
 	"fmt"
 	"strconv"
-	"strings"
 )
 
 //go:embed inputs/d12.in
 var b []byte
-
-// recursive from left
-// 2 paths: fill in the value, don't fill in
-// sum up the returns
-// if satisfies the constraints, return 1, if encounters issue, return 0
-// trim edges and make multi dots into single dot
-
-func options(line string, nrs []int, lineLen int, offset int, hcount int, xsum int, xorig int, trace int) int {
-	if lineLen == offset || xsum < 0 || len(nrs) == 0 {
-		if xsum == 0 {
-			elems := strings.Split(line, ".")
-			var nnrs []int
-			for _, e := range elems {
-				if e == "" {
-					continue
-				}
-				n := strings.Count(e, "#")
-				nnrs = append(nnrs, n)
-			}
-			if len(nnrs) != len(nrs) {
-				return 0
-			}
-			for i:=0; i<len(nrs); i++ {
-				if nnrs[i] != nrs[i] {
-					return 0
-				}
-			}
-			// fmt.Println(trace, line, hcount, nnrs, nrs)
-			return 1
-		}
-		return 0
-	}
-
-	switch line[offset] {
-	case '?':
-		l := []byte(line)
-		ll := []byte(line)
-		l[offset]='.'
-		ll[offset]='#'
-		uf := options(string(l), nrs, lineLen, offset, hcount, xsum, xorig, 1)
-		f := options(string(ll), nrs, lineLen, offset, hcount, xsum, xorig, 2)
-		return uf+f
-	case '#':
-		return options(line, nrs, lineLen, offset+1, hcount+1, xsum-1, xorig, trace)
-	case '.':
-		return options(line, nrs, lineLen, offset+1, hcount, xsum, xorig, trace)
-	}
-	return 0
-}
-		
-	
-
-func e1() int {
-	lines := strings.Split(string(b), "\n")
-	arrangements := 0
-	for _, line := range lines {
-		line = strings.Trim(line, ".")
-		line = strings.ReplaceAll(line, "..", ".")
-		elems := strings.Split(line, " ")
-		nrBs := strings.Split(elems[1], ",")
-		nsize := len(nrBs)
-		nrs := make([]int, nsize)
-		// fmt.Println(string(line))
-		nsum := 0
-		for i, nr := range nrBs {
-			nrs[i],_ = strconv.Atoi(string(nr))
-			nsum += nrs[i]
-		}
-		// break
-		arrangements +=options(elems[0], nrs, len(elems[0]), 0, 0, nsum, nsum, 0)
-	}
-	return arrangements
-}
-
 
 func walk(cond []byte, csize int, coffset int, grps []int, gsize int, goffset int, seen []int) int {
 	if coffset == csize && goffset - gsize <= 1 {
@@ -95,17 +20,6 @@ func walk(cond []byte, csize int, coffset int, grps []int, gsize int, goffset in
 		// fmt.Println("returning success 1:", string(cond), grps, gsize, seen, goffset, coffset)
  		return 1
 	}
-
-// 	for i, s := range seen {
-// 		break
-// 		if s > grps[i] {
-// 			// fmt.Println("returning failure 1:", string(cond), csize, grps, gsize, seen, goffset)
-// 			return 0
-// 		}
-// 		if s == 0 {
-// 			break
-// 		}
-// 	}
 
 	switch cond[coffset] {
 	case '?':
@@ -159,7 +73,7 @@ func ex(repeat int) int {
 		lNrbs := len(nrbs)
 		gsize := lNrbs*repeat
 		grps := make([]int, gsize)
-		seen := make([]int, gsize)
+		seen := make([]int, lNrbs)
 		gsum := 0
 		for i, nr := range nrbs {
 			grps[i], _ = strconv.Atoi(string(nr))
@@ -168,8 +82,17 @@ func ex(repeat int) int {
 			}
 			gsum += grps[i]*repeat
 		}
-		fmt.Println(string(cond), grps)
-		arrangements += walk(cond, csize, 0, grps, gsize, 0, seen)
+		// fmt.Println(string(cond), grps)
+		for i:=0; i<repeat; i++ {
+			sidx := i*lElems0+i
+			eidx := (i+1)*lElems0+(i+1)
+			if i==repeat-1 {
+				eidx--
+			}
+			partial := walk(cond[sidx:eidx], lElems0, 0, grps[i*lNrbs:(i+1)*lNrbs], lNrbs, 0, seen)
+			// fmt.Println("Partial:", partial)
+			arrangements += partial
+		}
 		// break
 	}
 	return arrangements
@@ -177,6 +100,6 @@ func ex(repeat int) int {
 
 
 func main() {
-	fmt.Printf("ex1: %v\n", ex(5))
-	// fmt.Printf("e1: %v\n", e1())
+	e1 := ex(1)
+	fmt.Printf("e1: %v\n", e1)
 }
